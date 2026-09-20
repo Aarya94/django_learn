edddf3d9cf2def3a433888991825f5ae0d903fb8
Aaryan Suman <aaryansuman17@gmail.com>
2026-09-20 07:36:45 +0530

    added home page

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, request
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {'calc':'Suman'})
+    return HttpResponse("Hello, welcome to the calculator app!!!")  
+    return render(request, 'home.html',{'name':'Aaryan'})
